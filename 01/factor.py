6bde42497b364ba40337d8e839a5f1fb6747513f
b3n-b3n <bencik42@gmail.com>
2022-02-15 10:17:31 +0100

    fix

--- 01/factor.py
+++ 01/factor.py
@@ -4,8 +4,10 @@ import math
 def main():
     with open("input.txt") as f:
         num = int(f.readline())
-        for i in range(1, int(math.sqrt(num))):
-            if num % i == 0: print(i)
+        if num <= 0: print('-')
+        else:
+            for i in range(1, int(math.sqrt(num))):
+                if num % i == 0: print(i)
 
 if __name__ == '__main__':
     main()
