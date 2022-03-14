9a796f715fc41bdaca622e2f36fe14c38468e9c5
bencikb <bencik42@gmail.com>
2022-03-14 13:50:32 +0100

    push whole repo

--- 04/override.sh
+++ 04/override.sh
@@ -5,4 +5,5 @@
 # if there is none echo "Error: HEADER not found"
 
 set -o pipefail
-test -f .NO_HEADER || cat HEADER 2> /dev/null || echo "Error: HEADER not found." >&2
+test -f .NO_HEADER || cat HEADER 2> /dev/null && echo "Error: HEADER not found." >&2
+# test -f .NO_HEADER || cat HEADER 2> /dev/null && echo
