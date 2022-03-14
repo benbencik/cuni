9d5f647ab7376a50f00aacf1ac162efdc706b108
bencikb <bencik42@gmail.com>
2022-03-14 22:27:52 +0100

    row sum

--- 04/row_sum.sh
+++ 04/row_sum.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # set -ueo pipefail
-tr '|' '0' | tr ' ' '+' | tr --squeeze-repeats '+' | bc
+# tr '|' '0' | tr ' ' '+' | tr --squeeze-repeats '+' | bc
 
 # cat <&0 | tr '|' '0' | tr ' ' '+' | tr --squeeze-repeats '+' | bc | cat
 # tr ' ' '|' < inp_row_sum | cat
 # cat - | tr ' ' '+' | echo
 
-# tr \| 0|tr -s \  + |bc
+tr \| 0|tr -s \  + |bc
