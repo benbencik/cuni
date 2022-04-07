38fcb2cfdfde9ae64340a75c32695350e97fdca5
bencikb <bencik42@gmail.com>
2022-04-07 22:04:55 +0200

    fix 08/ip.sh

--- 08/ip.sh
+++ 08/ip.sh
@@ -5,7 +5,7 @@ temp_dir="$( mktemp -d )"
 machine=''
 max_attempts=0
 for ip in $(grep -E "404" | cut -d ' ' -f 1); do
-    if $(test -f "$temp_dir/$ip"); then
+    if test -f "$temp_dir/$ip"; then
         current="$(cat "$temp_dir/$ip")"
         current=$((current + 1))
         echo $current > "$temp_dir/$ip"  
@@ -14,9 +14,9 @@ for ip in $(grep -E "404" | cut -d ' ' -f 1); do
     fi
 
     if [ "$(cat $temp_dir/$ip)" -gt $max_attempts ]; then
-        max_attempts=$(cat $temp_dir/$ip)
+        max_attempts=$(cat "$temp_dir/$ip")
         machine=$ip
     fi
 done
 
-echo $machine
+echo "$machine"
