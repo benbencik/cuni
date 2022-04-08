6185b889663d8721a812e5789f01c8a29ba5f35b
bencikb <bencik42@gmail.com>
2022-04-08 14:14:22 +0200

    add entities 08/markdown.sh

--- 08/markdown.sh
+++ 08/markdown.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -ueo pipefail
 
-# emphasis | strong | links | 
-sed 's#_\([a-z A-Z0-9]*\)_#<em>\1<\\em>#g' | 
-sed 's#\*\([a-z A-Z0-9]*\)\*#<strong>\1<\\strong>#g' | 
-sed 's#[(http://|https://)]\(*\)|\(*\)]#<a href="http://\1">\2</a>>#g' |
-sed 's#\[\(.*\)|\(.*\)\]#<a href="\1">\2</a>>#g' | sed 's#https://#http://#g'
+ 
+sed 's#_\([a-z A-Z0-9]*\)_#<em>\1</em>#g' | # emphasis
+sed 's#\*\([a-z A-Z0-9]*\)\*#<strong>\1</strong>#g' | # strong
+sed 's#\[\(.*\)|\(.*\)\]#<a href="\1">\2</a>>#g' | sed 's#https://#http://#g' | # link
+sed 's#&#&amp;#g' | sed 's#<#\&lt;#g' | sed 's#>#\&gt;#g'
 # (http://|https://)
 # <a href="http://"></a>>
