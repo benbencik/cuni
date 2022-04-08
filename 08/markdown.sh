7729cc4aa7a2c6cf1a31e3acb056cdcd01c431b6
bencikb <bencik42@gmail.com>
2022-04-08 14:17:39 +0200

    fix 08/markdown.sh

--- 08/markdown.sh
+++ 08/markdown.sh
@@ -2,9 +2,7 @@
 set -ueo pipefail
 
  
+sed 's#&#&amp;#g' | sed 's#<#\&lt;#g' | sed 's#>#\&gt;#g' | #entities
 sed 's#_\([a-z A-Z0-9]*\)_#<em>\1</em>#g' | # emphasis
 sed 's#\*\([a-z A-Z0-9]*\)\*#<strong>\1</strong>#g' | # strong
-sed 's#\[\(.*\)|\(.*\)\]#<a href="\1">\2</a>>#g' | sed 's#https://#http://#g' | # link
-sed 's#&#&amp;#g' | sed 's#<#\&lt;#g' | sed 's#>#\&gt;#g'
-# (http://|https://)
-# <a href="http://"></a>>
+sed 's#\[\(.*\)|\(.*\)\]#<a href="\1">\2</a>>#g' | sed 's#https://#http://#g' # link
