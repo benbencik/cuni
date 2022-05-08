77b5be5467ec087f9b8f266937c9091794a29a91
bencikb <bencik42@gmail.com>
2022-05-08 22:15:04 +0200

    setup

--- 11/tapsum2json/src/main.py
+++ 11/tapsum2json/src/main.py
@@ -11,7 +11,6 @@ def main():
     out = {"summary": []}
 
     for f in files[1:]:
-        print(f)
         res = {"filename": f, "total": 0, "passed": 0, "skipped": 0, "failed": 0}
         if (os.path.exists(f)):
             inp = parser.parse_file(f)
@@ -26,4 +25,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-    
