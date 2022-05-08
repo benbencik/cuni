89ef55bff9b3e4a9a86be7704856575a41f9723c
bencikb <bencik42@gmail.com>
2022-05-08 22:04:55 +0200

    remove venv

---
+++ 11/tapsum2json/src/main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import sys
+import json
+from tap.parser import Parser
+import os
+
+def main():
+    files = sys.argv
+    parser = Parser()
+    out = {"summary": []}
+
+    for f in files[1:]:
+        print(f)
+        res = {"filename": f, "total": 0, "passed": 0, "skipped": 0, "failed": 0}
+        if (os.path.exists(f)):
+            inp = parser.parse_file(f)
+            for line in inp:
+                if (line.category == "test"):
+                    res["total"] += 1
+                    if line.ok: res["passed"] += 1
+                    elif line.skip: res["skipped"] += 1 
+                    else: res["failed"] += 1
+        out["summary"].append(res)
+    print(json.dumps(out, indent=True))
+
+if __name__ == "__main__":
+    main()
+    
