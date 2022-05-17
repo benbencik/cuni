5c5528afec5d4e33e031c1c28642a9b69a8b5d89
benbencik <bencik42@gmail.com>
2022-05-17 15:14:32 +0200

    adding template code

--- 11_rojove algoritmy/aco_salesman/aco.py
+++ 11_rojove algoritmy/aco_salesman/aco.py
@@ -1,6 +1,7 @@
 import xml.etree.ElementTree as ET
-import sys
-import os
+import math
+import functools
+
 
 test = "data_32.xml"
 # test = "data_72.xml"
@@ -12,6 +13,8 @@ cat = {"info": 0, "network": 1, "fleet": 2, "requests": 3}  # categories
 # fleet info
 cap = -1
 for fleet in root[cat["fleet"]]: cap = fleet[2].text
+
+
 
 # graph
 warehouse = -1
@@ -25,3 +28,113 @@ for nodes in root[cat["network"]]:
 req = {}
 for request in root[cat["requests"]]:
     req[int(request.attrib["id"])] = {"dest": int(request.attrib["node"]), "size": float(request[0].text)}
+
+# --------------------------------------------------------------------------------------------------------
+
+@functools.lru_cache(maxsize=None)
+def distance(v1, v2):
+    return math.sqrt((v1.x - v2.x)**2+(v1.y - v2.y)**2)
+
+def fitness(vertices, distance, solution):
+    solution_distance = 0
+    for x, y in zip(solution, solution[1:]):
+        solution_distance += distance(vertices[x], vertices[y])
+    solution_distance += distance(vertices[solution[-1]], vertices[solution[0]])
+    return solution_distance
+
+def initialize_pheromone(N):
+    return 0.01*np.ones(shape=(N,N))
+
+def update_pheromone(pheromones_array, solutions, fits, Q=100, rho=0.6):
+    pheromone_update = np.zeros(shape=pheromones_array.shape)
+    for solution, fit in zip(solutions, fits):
+        for x, y in zip(solution, solution[1:]):
+            pheromone_update[x][y] += Q/fit
+        pheromone_update[solution[-1]][solution[0]] += Q/fit
+    
+    return (1-rho)*pheromones_array + pheromone_update
+
+
+def generate_solutions(vertices, pheromones, distance, N, alpha=1, beta=3):
+    
+    # pravdepodobnost vyberu dalsiho mesta
+    def compute_prob(v1, v2):
+        dist = 1/distance(vertices[v1], vertices[v2])
+        tau = pheromones[v1, v2]
+        ret = pow(tau, alpha) * pow(dist,beta)
+        return ret if ret > 0.000001 else 0.000001
+
+    pheromones_shape = pheromones.shape[0]
+    for i in range(N):
+        available = list(range(pheromones_shape))
+        solution = [np.random.randint(0, pheromones_shape)]
+        available.remove(solution[0])
+        while available:
+            probs = np.array(list(map(lambda x: compute_prob(solution[-1], x), available)))
+            selected = np.random.choice(available, p=probs/sum(probs)) # vyber hrany
+            solution.append(selected)
+            available.remove(selected)
+        yield solution
+
+
+def ant_solver(vertices, distance, ants=10, max_iterations=3000, alpha=1, beta=3, Q=100, rho=0.8):
+    pheromones = initialize_pheromone(len(vertices))
+    best_solution = None
+    best_fitness = float('inf')
+    
+    for i in range(max_iterations):
+        solutions = list(generate_solutions(vertices, pheromones, distance, ants, alpha=alpha, beta=beta))
+        fits = list(map(lambda x: fitness(vertices, distance, x), solutions))
+        pheromones = update_pheromone(pheromones, solutions, fits, Q=Q, rho=rho)
+        
+        for s, f in zip(solutions, fits):
+            if f < best_fitness:
+                best_fitness = f
+                best_solution = s
+        
+        print(f'{i:4}, {np.min(fits):.4f}, {np.mean(fits):.4f}, {np.max(fits):.4f}')
+    return best_solution, pheromones
+
+vertices = []
+with open('cities.csv') as cities_file:
+    csv_reader = csv.reader(cities_file, delimiter=',')
+    for row in csv_reader:
+        vertices.append(Vertex(row[0], float(row[2]), float(row[1])))
+
+pprint.pprint(vertices)
+
+best_solution, pheromones = ant_solver(vertices, distance)
+
+
+
+
+lines = []
+colors = []
+for i, v1 in enumerate(vertices):
+    for j, v2 in enumerate(vertices):
+        lines.append([(v1.x, v1.y), (v2.x, v2.y)])
+        colors.append(pheromones[i][j])
+
+lc = mc.LineCollection(lines, linewidths=np.array(colors))
+
+plt.figure(figsize=(12, 8))
+ax = plt.gca()
+ax.add_collection(lc)
+ax.autoscale()
+
+solution = best_solution
+
+# tady muzeme zkouset vliv jednotlivych parametru na vygenerovane reseni
+# solution = list(generate_solutions(vertices, pheromones, distance, N=1, alpha=3, beta=1))[0]
+
+print('Fitness: ', fitness(vertices, distance, solution))
+
+solution_vertices = [vertices[i] for i in solution]
+pprint.pprint(solution_vertices)
+
+solution_lines = []
+for i, j in zip(solution, solution[1:]):
+    solution_lines.append([(vertices[i].x, vertices[i].y), (vertices[j].x, vertices[j].y)])
+solution_lines.append([(vertices[solution[-1]].x, vertices[solution[-1]].y), (vertices[solution[0]].x, vertices[solution[0]].y)])
+solutions_lc = mc.LineCollection(solution_lines, colors='red')
+ax.add_collection(solutions_lc)
