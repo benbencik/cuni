9ad389eb917d6806c4d21208de5a97e00fa09097
benbencik <bencik42@gmail.com>
2022-05-17 14:50:52 +0200

    parsing into dictionaries

--- 11_rojove algoritmy/aco_salesman/aco.py
+++ 11_rojove algoritmy/aco_salesman/aco.py
@@ -7,16 +7,21 @@ test = "data_32.xml"
 # test = "data_422.xml"
 tree = ET.parse(f"data/{test}")
 root = tree.getroot()
-
 cat = {"info": 0, "network": 1, "fleet": 2, "requests": 3}  # categories
-# n_attr = {"nodes": 0, "euclidean": 1, "decimals": 2}  # network_attributes
 
 # fleet info
-for child in root[1]:
-    for ch in child:
-        # print(ch.tag, ch.attrib)
-        print(ch.attrib["id"])
-        print(ch[0].text)
+cap = -1
+for fleet in root[cat["fleet"]]: cap = fleet[2].text
 
-# load graph
+# graph
+warehouse = -1
+graph = {}
+for nodes in root[cat["network"]]:
+    for node in nodes:
+        if node.attrib["id"] == "0": warehouse = int(node.attrib["id"])
+        graph[int(node.attrib["id"])] = {"x": node[0].text, "y": node[1].text}
 
+# requests
+req = {}
+for request in root[cat["requests"]]:
+    req[int(request.attrib["id"])] = {"dest": int(request.attrib["node"]), "size": float(request[0].text)}
