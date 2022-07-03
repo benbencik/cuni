dae2baf69e863dd1cd415c03916eff87c145eaf4
bencikb <bencik42@gmail.com>
2022-07-04 00:25:32 +0200

    commenting out old code

--- 06/barplot.sh
+++ 06/barplot.sh
@@ -1,31 +1,31 @@
-#!/bin/bash
+# #!/bin/bash
 
-set -eu
+# set -eu
 
-plot_line() {
-    for i in $(seq "$inp"); do
-    echo "$i" > /dev/null
-        echo -n '#'
-    done
-}
+# plot_line() {
+#     for i in $(seq "$inp"); do
+#     echo "$i" > /dev/null
+#         echo -n '#'
+#     done
+# }
 
-max=0;
+# max=0;
 
-for inp in "$@"; do
-    if [ "$inp" -ge "$max" ]; then
-        max=$inp
-    fi
-done
+# for inp in "$@"; do
+#     if [ "$inp" -ge "$max" ]; then
+#         max=$inp
+#     fi
+# done
 
-if [ "$max" -ge 60 ]; then
-    for inp in "$@"; do
-        plot_line "$inp"
-        echo
-    done
-else
-    ratio="$(( max / 60 ))"
-    for inp in "$@"; do
-        plot_line "$(( inp * ratio ))"
-        echo
-    done
-fi
+# if [ "$max" -ge 60 ]; then
+#     for inp in "$@"; do
+#         plot_line "$inp"
+#         echo
+#     done
+# else
+#     ratio="$(( max / 60 ))"
+#     for inp in "$@"; do
+#         plot_line "$(( inp * ratio ))"
+#         echo
+#     done
+# fi
--- 06/plot.sh
+++ 06/plot.sh
@@ -1,58 +1,58 @@
-#!/bin/bash
-set -ueo pipefail
+# #!/bin/bash
+# set -ueo pipefail
 
-repeat_string() {
-    local i
-    for i in $( seq $1 ); do
-        echo -n "$2"
-    done
-}
+# repeat_string() {
+#     local i
+#     for i in $( seq $1 ); do
+#         echo -n "$2"
+#     done
+# }
 
-temp_dir="$( mktemp -d )"
-longest_bar=""
+# temp_dir="$( mktemp -d )"
+# longest_bar=""
 
-# COLUMNS=20
-# if there is no value set it to default
-if [ -z $COLUMNS ]; then
-    COLUMNS=80
-fi 
-
-
-while read len name || [ -n "$len" ]; do
-    # if [ "$len" == "#" ]; then
-    #     screen_width="$(echo $name | cut -d "=" -f 2)"
-    # else
-
-    # load values into file
-    echo "$len $name" >> $temp_dir/data
-    # set the maximum
-    [ -z "$longest_bar" ] && longest_bar="$len"
-    if [ "$len" -gt "$longest_bar" ]; then
-        longest_bar="$len";
-    fi
-done < $1
+# # COLUMNS=20
+# # if there is no value set it to default
+# if [ -z $COLUMNS ]; then
+#     COLUMNS=80
+# fi 
 
 
-longest_label="$(wc -L "$temp_dir/data" | cut -d " " -f 1)"
-longest_label="$(( longest_label + 5))"
-# actual space dedicated for the bar
-space_for_bar="$(( "$COLUMNS" - "$longest_label" ))"
+# while read len name || [ -n "$len" ]; do
+#     # if [ "$len" == "#" ]; then
+#     #     screen_width="$(echo $name | cut -d "=" -f 2)"
+#     # else
 
-# scaling of the bar
-if [ "$longest_bar" -gt "$space_for_bar" ]; then
-    # do not compute for better precision later on
-    scale="$space_for_bar / $longest_bar"
-else
-    scale="$space_for_bar / $longest_bar"
-fi
+#     # load values into file
+#     echo "$len $name" >> $temp_dir/data
+#     # set the maximum
+#     [ -z "$longest_bar" ] && longest_bar="$len"
+#     if [ "$len" -gt "$longest_bar" ]; then
+#         longest_bar="$len";
+#     fi
+# done < $1
 
 
-while read len name || [ -n "$len" ]; do
-    # $scale must not be quoted (it may contain an expression)
-    size="$(( $len * $scale ))"
+# longest_label="$(wc -L "$temp_dir/data" | cut -d " " -f 1)"
+# longest_label="$(( longest_label + 5))"
+# # actual space dedicated for the bar
+# space_for_bar="$(( "$COLUMNS" - "$longest_label" ))"
 
-    label_len="$( echo $len $name | wc -m)"
-    label_len="$(( $label_len+4 ))"
-    additional_space="$(( $longest_label - $label_len ))"
-    printf "%s (%d) %s| %s\n" "$name" "$len" "$( repeat_string "$additional_space" " " )" "$( repeat_string "$size" "#" )"
-done < "$temp_dir/data"
+# # scaling of the bar
+# if [ "$longest_bar" -gt "$space_for_bar" ]; then
+#     # do not compute for better precision later on
+#     scale="$space_for_bar / $longest_bar"
+# else
+#     scale="$space_for_bar / $longest_bar"
+# fi
+
+
+# while read len name || [ -n "$len" ]; do
+#     # $scale must not be quoted (it may contain an expression)
+#     size="$(( $len * $scale ))"
+
+#     label_len="$( echo $len $name | wc -m)"
+#     label_len="$(( $label_len+4 ))"
+#     additional_space="$(( $longest_label - $label_len ))"
+#     printf "%s (%d) %s| %s\n" "$name" "$len" "$( repeat_string "$additional_space" " " )" "$( repeat_string "$size" "#" )"
+# done < "$temp_dir/data"
