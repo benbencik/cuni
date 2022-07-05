5a7c1ab5549bc235aa994ceef84f617c489534c3
bencikb <bencik42@gmail.com>
2022-07-05 09:38:25 +0200

    packing with python3

--- 06/scoring.sh
+++ 06/scoring.sh
@@ -1,28 +1,26 @@
-# #!/bin/bash
-# set -ueo pipefail
+#!/bin/bash
+set -ueo pipefail
 
-# temp_dir="$( mktemp -d )"
-# is_recorded=0
+temp_dir="$( mktemp -d )"
 
-# # calculate the final results 
-# while read -r scores; do
-#     is_recorded=0
-#     team=$( echo "$scores" | tr --squeeze-repeats ' ' | cut -d ' ' -f 1)
-#     score=$( echo "$scores" | tr --squeeze-repeats ' ' | cut -d ' ' -f 2)
+# calculate the final results 
+while read -r scores; do
+    team=$( echo "$scores" | tr --squeeze-repeats ' ' | cut -d ' ' -f 1)
+    score=$( echo "$scores" | tr --squeeze-repeats ' ' | cut -d ' ' -f 2)
 
-#     if $(test -f "$temp_dir/$team"); then
-#         current="$(cat "$temp_dir/$team")"
-#         current=$((current + score))
-#         echo $current > "$temp_dir/$team"  
-#     else
-#         echo $score > "$temp_dir/$team"
-#     fi
-# done
+    if test -f "$temp_dir/$team"; then
+        current=$(cat "$temp_dir/$team")
+        current=$((current + score))
+        echo $current > "$temp_dir/$team"  
+    else
+        echo "$score" > "$temp_dir/$team"
+    fi
+done
 
-# # print results
-# for team in $temp_dir/*; do
-#     value="$(cat $team)"
-#     team_name="$(echo $team | rev | cut -d '/' -f 1 | rev)"
-#     echo "$team_name:$value"
-# done
+# print results
+for team in "$temp_dir"/*; do
+    value="$(cat "$team")"
+    team_name="$(echo "$team" | rev | cut -d '/' -f 1 | rev)"
+    echo "$team_name:$value"
+done
 
--- 06/sysinfo.sh
+++ 06/sysinfo.sh
@@ -1,66 +1,66 @@
-# #!/bin/bash
-# set -ueo pipefail
+#!/bin/bash
+set -ueo pipefail
 
-# load="$(cat /proc/loadavg | cut -d ' ' -f 1)"
-# kernel="$(uname -r)"
-# cpus="$(nproc)"
-# res=""
-# l=0; k=0; c=0; s=0; h=0
-# args_num="$#"
+load="$( /proc/loadavg | cut -d ' ' -f 1)"
+kernel="$(uname -r)"
+cpus="$(nproc)"
+res=""
+l=0; k=0; c=0; s=0; h=0
+args_num="$#"
 
-# opts_short="lkchs"
-# opts_long="load,kernel,cpus,help,script"
-# getopt -Q -o "$opts_short" -l "$opts_long" -- "$@" || exit 1
-# eval set -- "$( getopt -o "$opts_short" -l "$opts_long" -- "$@" )"
+opts_short="lkchs"
+opts_long="load,kernel,cpus,help,script"
+getopt -Q -o "$opts_short" -l "$opts_long" -- "$@" || exit 1
+eval set -- "$( getopt -o "$opts_short" -l "$opts_long" -- "$@" )"
 
-# if [ $args_num -eq 0 ]; then
-#     l=1; k=1; c=1
-# else
-#     while [ $# -gt 0 ]; do
-#         case "$1" in
-#             -l | --load) l=1;;
-#             -k | --kernel) k=1;;
-#             -c | --cpus) c=1;;
-#             -s | --script) 
-#                 s=1
-#                 # if there are no other arguments
-#                 if [ $args_num -eq 1 ]; then
-#                     l=1; k=1; c=1
-#                 fi
-#                 ;;
-#             -h | --help) h=1;;
-#         esac
-#         shift
-#     done
-# fi
+if [ $args_num -eq 0 ]; then
+    l=1; k=1; c=1
+else
+    while [ $# -gt 0 ]; do
+        case "$1" in
+            -l | --load) l=1;;
+            -k | --kernel) k=1;;
+            -c | --cpus) c=1;;
+            -s | --script) 
+                s=1
+                # if there are no other arguments
+                if [ $args_num -eq 1 ]; then
+                    l=1; k=1; c=1
+                fi
+                ;;
+            -h | --help) h=1;;
+        esac
+        shift
+    done
+fi
 
-# # append to the res in desired order
-# if [ "$l" -eq 1 ]; then
-#     res="${res}load=$load "
-# fi
-# if [ "$k" -eq 1 ]; then
-#     res="${res}kernel=$kernel "
-# fi
-# if [ "$c" -eq 1 ]; then
-#     res="${res}cpus=$cpus "
-# fi
+# append to the res in desired order
+if [ "$l" -eq 1 ]; then
+    res="${res}load=$load "
+fi
+if [ "$k" -eq 1 ]; then
+    res="${res}kernel=$kernel "
+fi
+if [ "$c" -eq 1 ]; then
+    res="${res}cpus=$cpus "
+fi
 
-# # remove the last character
-# res=${res::-1}
+# remove the last character
+res=${res::-1}
 
-# # printing
-# if [ "$h" -eq 1 ]; then
-#     echo "Usage: sysinfo [options]"
-#     echo " -c   --cpu     Print number of CPUs."
-#     echo " -l   --load    Print current load."
-#     echo " -k   --kernel  Print kernel version."
-#     echo " -s   --script  Each value on separate line."
-#     echo ""
-#     echo "Without arguments behave as with -c -l -k."
-#     echo ""
-#     echo "Copyright NSWI177 2022"
-# elif [ "$s" -eq 1 ]; then
-#     echo "$res" | tr ' ' '\n'
-# else
-#     echo $res
-# fi
+# printing
+if [ "$h" -eq 1 ]; then
+    echo "Usage: sysinfo [options]"
+    echo " -c   --cpu     Print number of CPUs."
+    echo " -l   --load    Print current load."
+    echo " -k   --kernel  Print kernel version."
+    echo " -s   --script  Each value on separate line."
+    echo ""
+    echo "Without arguments behave as with -c -l -k."
+    echo ""
+    echo "Copyright NSWI177 2022"
+elif [ "$s" -eq 1 ]; then
+    echo "$res" | tr ' ' '\n'
+else
+    echo "$res"
+fi
