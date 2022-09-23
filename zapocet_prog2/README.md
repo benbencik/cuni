e61df7328f9e1d227e823288320866b3cedc2804
bencikb <bencik42@gmail.com>
2022-09-23 19:52:14 +0200

    almost there

--- zapocet_prog2/README.md
+++ zapocet_prog2/README.md
@@ -1,60 +1,67 @@
-# Fast Fourier Transform
-
+# Demonstration of Discrete Fourier Transform
+ 
 Running the application does not require any special setup, besides installing libraries mentioned in *requirements.txt*
-
-The application can be in 3 different states: *idle*, *recording trace*, *running*. User can itterate forward with SPACE and rest the STATE to *idle* with R key. If the trace is taken from a svg file the application directly skipps to stage *running* 
-
+ 
+The application can be in 3 different states: *idle*, *recording trace*, *running*. User can iterate forward with SPACE and rest the STATE to *idle* with R key. If the trace is taken from a svg file the application directly skipps to stage *running*
+ 
 #### Comandline arguments
-* **set screen resoluition:** -r width height
-* **input file:** -i image will scaled to screen
-* **number of approximation functions:** -n
-* **sampling rate for tracing mouse cursor:** -s
-* **point density in svg file per path:** -d
-* **window update rate:** -u
-
+* *set screen resolution:* -r width height
+* *input file:* -i image will scaled to screen
+* *number of approximation functions:* -n
+* *sampling rate for tracing mouse cursor:* -s
+* *point density in svg file per path:* -d
+* *window update rate:* -u
+ 
 #### GUI (mentioned in the welcome screen)
-* **quitting the app:** ESC
-* **toggling elements form view**
-  * traced points: P
-  * vector lines: L
-  * circle on which the vector revolves: C
-* **zooming** (can speed up by holding the key)
-  * zoom-in: Z
-  * zoom-out: X
-
+* *quitting the app:* ESC
+* *toggling elements form view*
+ * traced points: P
+ * vector lines: L
+ * circle on which the vector revolves: C
+* *zooming* (can speed up by holding the key)
+ * zoom-in: ↑
+ * zoom-out: ↓
+* *#vectors*
+  * increase: → 
+  * decrease: ←
+   
 ## Documentation
-The application is all inside one module and has a standard pygame structure with infinite loop. The funcitonality relies on two classes.
-
+The application is all inside one module and has a standard pygame structure with infinite loop. The functionality relies on two classes.
+ 
 #### Window
-Is responsible for the graphic interface. Creates the welcome screen as well draws grid in the begining. Nothing interesting.
-
-#### FouriesSeries
+Is responsible for the graphic interface. Presents the welcome screen as well as draws a grid in the beginning and contains all information about the "canvas".
+ 
+#### Fourier Transform
 Does all the calculation. The two main functions are:
 * *calculate_coefficienet*: takes the traced points and calculates appropriate coefficient $c_k$ for each function
-* *calculate_point*: is called in every itteration of t, it calculates the resulting point which is drawn by the tip of the last vector. It simultaneously draws the vectores and circle on which they rotate
-
-## How does it work
-The application tries to demonstrate that an arbitrary curve could be generated as a sum of infinite functions. Since it is not possible to perform infinite calcuilation we can just the show, that by rasing the number of function we can get a pretty close approximaion of the former curve. 
-
-The computation happen on complex plane and all of the rotation are described by expression $c \cdot e^{2\pi i t}$, where the coeficient $c$ determines initial angle and magnitude of the rotating vector. $t$ represents time in rotation
-
-$$ c_n e^{freq \cdot time \cdot 2\pi i}$$
-
-Our goal is to be able to express approximation of an arbitrary curve in form of function:
-$$ f(t) = c_{-n} e^{-n \cdot t2\pi i} + ... + c_{0} e^{0} + ... + c_{n} e^{n \cdot t2\pi i}$$
-
-Therefore we need to be able to calculate coeficients $c$.
-
+* *calculate_point*: is called in every iteration of t, it calculates the resulting point which is drawn by the tip of the last vector. It simultaneously draws the vectores and circle on which they rotate
+ 
+## The idea behind it
+The application tries to demonstrate that an arbitrary curve could be generated as a sum of infinite functions. Since it is not possible to perform infinite calculation we can just show that by raising the number of functions we can get a pretty close approximation of the former curve. First we will look at the formal infinite definition and then discretize it.
+ 
+The computation happens in a complex plane and all of the rotations are described by expression $c \cdot e^{freq \cdot time \cdot 2\pi i}$, where the coefficient $c$ determines initial angle and magnitude of the rotating vector. Our goal is to be able to express approximation of an arbitrary curve in form of function:
+ 
+$$ f(t) = c_{-n} e^{-n \cdot t2\pi i} + ... + c_{-1} e^{-1 \cdot t2\pi i} + c_{0} e^{0} + c_{1} e^{1 \cdot t2\pi i} + ... + c_{n} e^{n \cdot t2\pi i}$$
+ 
+Therefore we need to be able to calculate coefficients $c$. The easiest to find is the constant $c_0$, since the vector is not rotating the value of $c_0$ is equal to the average of all traced points. With finer distribution of points we will get the term $c_0$ in the limit. This infinite sum can be described as the integral $\int_0^1{f(t)dt}$. To get the average we would normally divide the sum by length of the input range, but that is equal to 1.
+ 
+$$\int_0^1{f(t)dt} = \int_0^1{c_{-n} e^{-n \cdot t2\pi i}} + ... + \int_0^1{c_{0} e^{0 \cdot t2\pi i}} + ... + \int_0^1{c_{n} e^{n \cdot t2\pi i}}$$
+ 
+We are able to split the integral and look at each term separately as the $t$ goes from zero to one. Every vector except the one with $c_0$ will make a whole number of rotations around the center, therefore their averages would be 0. Since $e^{0 \cdot t2\pi i}$ stays static the value $c_0$ would just be the position on which the vector has started. Now we can see that $\int_0^1{f(t)dt}$ in fact equals $c_0$.
+ 
+This neat trick can be further generalized to calculate any arbitrary coefficient $k$. The idea is to multiply the whole function by the expression which would stabilize the vector $k$. So, to *"kill"* the exponent in $c_k e^{k \cdot t2\pi i}$ needs to be multiplied by $e^{-k \cdot t2\pi i}$. We can be certain that there would not be any other static vector. The rotating frequencies for a  $\{-n, -n+1, ..., 0, ... n-1, n\}$ after applying $e^{-k \cdot t2\pi i}$ the set would shift and there would remain just one vector with the zero frequency. Finally we get formula:
+ 
 $$c_k = \int_0^1{f(t)e^{-k \cdot t2\pi i}}$$
-
-Or in the discrete form:
-
-$$c_k = \frac{1}{n} \sum_{l=0}^{n-1}c_l \cdot e^{-k \cdot 2\pi i}$$
-
-
-need to get the average point with really fine sum, that is basically an intrgral
-
+ 
+However, we will need to implement a discrete fourier transform. If we consider $p$ as the set of traced points then the coefficients could be computed like this:
+ 
+$$c_k = \frac{1}{n} \sum_{t=0}^{n-1}p_t \cdot e^{-k \cdot \frac{t}{n} 2\pi i}$$
+ 
+Why  bother with moving this into a complex plane? The computation is just cleaner in comparison to:
+$$c_k = \frac{1}{n} \sum_{t=0}^{n-1} p_t (\cos(k \cdot \frac{t}{n} 2\pi i) - i\sin(k \cdot \frac{t}{n} 2\pi i))$$
+ 
+ 
 ## Sources and Libraries
-I got inspiration for this project from wonderful youtube [video by 3b1b](https://www.youtube.com/watch?v=r6sGWTCMz2k&t=1298s). Calculations of the curve approximation were performed as explained in the video. 
-
-Two non-standar libraries were used in this project. *Pygame* for graphical interface and *svgpathtools* for parsing .svg file, besides that all code was written by me.
+I got inspiration for this project from wonderful youtube [video by 3b1b](https://www.youtube.com/watch?v=r6sGWTCMz2k&t=1298s). Calculations of the curve approximation were performed as explained in the video.
+ 
+Two non-standard libraries were used in this project. *Pygame* for graphical interface and *svgpathtools* for parsing .svg file, besides that all code was written by me.
--- zapocet_prog2/dft.py
+++ zapocet_prog2/dft.py
@@ -8,7 +8,6 @@ import argparse
 import svgpathtools
 
 
-
 class Window():
     def __init__(self, resolution) -> None:
         pygame.init()
@@ -70,7 +69,7 @@ class Window():
             'C, L, P to toggle circles, lines, points',
             'press SPACE to start/stop drawing',
             'ESC to exit R to reset',
-            'z/x to zoom in/out'
+            '↑/↓ to zoom in/out'
         ]
         ypos = canvas.height // 2 - 100
         for i in range(len(message)):
@@ -79,7 +78,8 @@ class Window():
                 text, ((self.width - text.get_width())//2, ypos + i*text.get_height()))
 
     def print_zoom(self, num):
-        text = self.font_info.render(f"Zoom: {round(num, 2)}", True, self.font_color)
+        text = self.font_info.render(
+            f"Zoom: {round(num, 2)}", True, self.font_color)
         self.display.blit(text, (20, self.width/30))
 
     def reset(self):
@@ -101,6 +101,7 @@ class FourierTransform():
 
         self.time = 0
         self.trace = []
+        self.trace_l = 0
         self.aprox_trace = []
         self.coefficients = []
         self.draw_last_line = False
@@ -113,84 +114,96 @@ class FourierTransform():
         self.offset = [0, 0]
 
     def calculate_coeficients(self):
+        self.trace_l = len(self.trace)
         # condition needs to be satisfied otherwise the algorithm malfunctions
         # for higher percision increase number of points traced
-        if len(self.trace) < self.n*2: self.n = len(self.trace)//2 
+        if self.trace_l < self.n*2:
+            self.n = self.trace_l//2
 
-        # transpose points 
-        for i in range(len(self.trace)):
+        # transpose points
+        for i in range(self.trace_l):
             x, y = self.trace[i]
             self.trace[i] = (x-canvas.width//2, y-canvas.height//2)
 
         # coeficients deretmine initial angle and magnitude
         self.coefficients = []
-        for freq in range(self.n, -self.n-1, -1):
-            l = []
-            for t in range(len(self.trace)):
-                num1 = cmath.exp(2*cmath.pi * 1j * freq * t/len(self.trace))
+        # for k in range(self.n, -self.n-1, -1):
+        for k in range(-self.n, self.n+1):
+            print(k)
+            sum_of_terms = 0
+            for t in range(self.trace_l):
+                num1 = cmath.exp(-k* t/self.trace_l * 2*cmath.pi * 1j)
                 num2 = (self.trace[t][0] + self.trace[t][1] * 1j)
-                l.append(num1*num2)
-            self.coefficients.append(sum(l)/len(self.trace))
+                sum_of_terms += num1*num2
+            self.coefficients.append(sum_of_terms/self.trace_l)
+        
 
     def calculate_point(self):
         z = canvas.width//2 + canvas.height//2*1j
-        
+
         # zip together frequencies by positive negative pairs
         frequency = [0]
         neg_range = range(-1, -self.n-1, -1)
         pos_range = range(1, self.n+1)
-        for r in zip(neg_range, pos_range): frequency += r
+        for r in zip(neg_range, pos_range):
+            frequency += r
 
         for freq in frequency:
             old_z = z
             # coeficient defining stgarting angle and magnitude
             complex_coef = self.coefficients[freq+self.n]
-            ttime = self.time / len(self.trace)
+            ttime = self.time / self.trace_l
             # add new function to resulting complex point
             z += complex_coef * cmath.exp(freq * ttime * 2*cmath.pi*1j)
-            
+
             # draw resulting vector
             p1 = self.zoom_point(old_z)
             p2 = self.zoom_point(z)
             if self.toggle_lines:
-                pygame.draw.line(canvas.display, canvas.color['grey'], 
-                                (int(p1.real), int(p1.imag)), (int(p2.real), int(p2.imag))) 
+                pygame.draw.line(canvas.display, canvas.color['grey'],
+                                 (int(p1.real), int(p1.imag)), (int(p2.real), int(p2.imag)))
             if self.toggle_circles:
-                r = cmath.sqrt((p1.real - p2.real)**2 + (p1.imag - p2.imag)**2).real
+                r = cmath.sqrt((p1.real - p2.real)**2 +
+                               (p1.imag - p2.imag)**2).real
                 if r > 1:
-                    pygame.draw.circle(canvas.display, canvas.color['grey'], 
-                                    (int(p1.real), int(p1.imag)), int(r), 1)
+                    pygame.draw.circle(canvas.display, canvas.color['grey'],
+                                       (int(p1.real), int(p1.imag)), int(r), 1)
 
         self.center = [z.real, z.imag]
-        self.offset = [self.center[0]-canvas.width/2, self.center[1]-canvas.height/2]
-        if len(self.aprox_trace) < len(self.trace): self.aprox_trace.append(z)
+        self.offset = [self.center[0]-canvas.width /
+                       2, self.center[1]-canvas.height/2]
+        if len(self.aprox_trace) < self.trace_l:
+            self.aprox_trace.append(z)
 
     def draw_curve(self):
         range_end = len(self.aprox_trace)
         if self.draw_last_line:
             range_end = len(self.aprox_trace)+1
-        elif self.time == len(self.trace)-1:
+        elif self.time == self.trace_l-1:
             self.draw_last_line = True
-        
+
         # draw approximated curve
         for i in range(1, range_end):
-            color = canvas.fade_color(i, self.time, len(self.trace))
+            color = canvas.fade_color(i, self.time, self.trace_l)
             p1 = self.zoom_point(self.aprox_trace[i-1])
-            p2 = self.zoom_point(self.aprox_trace[i%len(self.aprox_trace)])       
-            pygame.draw.line(canvas.display, color, (int(p1.real), int(p1.imag)), (int(p2.real), int(p2.imag)))
+            p2 = self.zoom_point(self.aprox_trace[i % len(self.aprox_trace)])
+            pygame.draw.line(canvas.display, color, (int(
+                p1.real), int(p1.imag)), (int(p2.real), int(p2.imag)))
 
         # draw traced points
         if self.toggle_points:
             for p in self.trace:
-                p = self.zoom_point(p[0]+canvas.width/2 + 1j*(p[1]+canvas.height/2))
-                canvas.display.set_at((int(p.real), int(p.imag)), canvas.trace_color)
+                p = self.zoom_point(p[0]+canvas.width /
+                                    2 + 1j*(p[1]+canvas.height/2))
+                canvas.display.set_at(
+                    (int(p.real), int(p.imag)), canvas.trace_color)
 
-        self.time = (self.time+1) % len(self.trace)
-    
+        self.time = (self.time+1) % self.trace_l
+
     def zoom_point(self, point):
-        if self.zoom == 1: return point
+        if round(self.zoom, 2) == 1:
+            return point
         return (self.center[0]*(1 - self.zoom) + point.real * self.zoom) + 1j*(self.center[1]*(1 - self.zoom) + point.imag * self.zoom)
-        
 
     def record_point(self):
         pos = pygame.mouse.get_pos()
@@ -205,38 +218,48 @@ class FourierTransform():
         self.time = 0
         self.draw_last_line = False
         self.zoom = 1
+        self.toggle_circles = True
+        self.toggle_lines = True
+        self.toggle_points = True
 
 
 def extract_svg():
-    paths, attributes, svg_attributes = svgpathtools.svg2paths2(args.input_file)
+    attributes, svg_attributes = svgpathtools.svg2paths2(args.input_file)[1:]
     image_w, image_h = map(int, svg_attributes['viewBox'].split(' ')[2:])
-    
+
     for attr in attributes:
         # extracting only the features wich are strokes
         if attr.get('d') and attr.get('stroke'):
             svgpath = attr['d']
             path = svgpathtools.parse_path(svgpath)
             POINTS_PER_PATH = args.point_density
-            
+
             # sample points from the line
             for i in range(0, POINTS_PER_PATH+1):
                 f = i/POINTS_PER_PATH
                 complex_point = path.point(f)
                 # append points transposed to canvas size
-                ft.trace.append((complex_point.real/image_w*canvas.width,
+                fs.trace.append((complex_point.real/image_w*canvas.width,
                                 complex_point.imag/image_h*canvas.height))
 
+
 arg_parser = argparse.ArgumentParser()
-arg_parser.add_argument('-r', '--resolution', nargs=2, help='set width and height of the window, default is fullscreen')
-arg_parser.add_argument('-i', '--input-file', type=str, help='specify a .svg file for trace')
-arg_parser.add_argument('-n', '--num-of-functions', type=int, default=100, help='number of functions used to approximate the curve')
-arg_parser.add_argument('-s', '--sampling-rate', type=int, default=100, help='mark trace of curve every n miliseconds')
-arg_parser.add_argument('-u', '--update-rate', type=int, default=50, help='one frame will take n miliseconds')
-arg_parser.add_argument('-d', '--point-density', type=int, default=200, help='number of point samples per path')
+arg_parser.add_argument('-r', '--resolution', nargs=2,
+                        help='set width and height of the window, default is fullscreen')
+arg_parser.add_argument('-i', '--input-file', type=str,
+                        help='specify a .svg file for trace')
+arg_parser.add_argument('-n', '--num-of-functions', type=int,
+                        default=100, help='number of functions used to approximate the curve')
+arg_parser.add_argument('-s', '--sampling-rate', type=int,
+                        default=100, help='mark trace of curve every n miliseconds')
+arg_parser.add_argument('-u', '--update-rate', type=int, default=50,
+                        help='one frame will take n miliseconds')
+arg_parser.add_argument('-d', '--point-density', type=int,
+                        default=200, help='number of point samples per path')
 args = arg_parser.parse_args()
 
 canvas = Window(args.resolution)
-ft = FourierTransform(canvas.width, canvas.height, args.num_of_functions)
+fs = FourierTransform(canvas.width, canvas.height, args.num_of_functions)
 
 """ 
 application might be in 3 different STATEs
@@ -249,9 +272,9 @@ SAMPLE_RATE = args.sampling_rate / 10e3  # mark trace every n miliseconds
 UPDATE_RATE = args.update_rate / 10e2  # move through time everty n miliseconds
 HOLD_AFTER = 0.5  # hold the zoom keys after n seconds
 
-if args.input_file: 
+if args.input_file:
     extract_svg()
-    ft.calculate_coeficients()
+    fs.calculate_coeficients()
     STATE = 2  # trace is recorded skip 1 state
 else:
     canvas.draw_grid()
@@ -274,52 +297,58 @@ while True:
                 canvas.display.fill(canvas.bg_color)
                 if STATE == 0:
                     canvas.draw_grid()
-                    ft.trace = [pygame.mouse.get_pos()]
+                    fs.trace = [pygame.mouse.get_pos()]
                     prev_time = current_time
                 if STATE == 1:
-                    ft.calculate_coeficients()
+                    fs.calculate_coeficients()
                 STATE += 1
             if e.key == pygame.K_r:
                 STATE = 0
                 canvas.reset()
-                ft.reset()
+                fs.reset()
             if e.key == pygame.K_p:
-                ft.toggle_points = not ft.toggle_points
+                fs.toggle_points = not fs.toggle_points
             if e.key == pygame.K_l:
-                ft.toggle_lines = not ft.toggle_lines
+                fs.toggle_lines = not fs.toggle_lines
             if e.key == pygame.K_c:
-                ft.toggle_circles = not ft.toggle_circles
-            if e.key == pygame.K_z:
-                ft.zoom += zoom_amount
+                fs.toggle_circles = not fs.toggle_circles
+            if e.key == pygame.K_UP:
+                fs.zoom += zoom_amount
                 press_z = current_time
-            if e.key == pygame.K_x:
-                if ft.zoom - zoom_amount > 0:
-                    ft.zoom -= zoom_amount
+            if e.key == pygame.K_DOWN:
+                if fs.zoom - zoom_amount > 0:
+                    fs.zoom -= zoom_amount
                 press_x = current_time
+            if e.key == pygame.K_RIGHT:
+                fs.n += 1
+                fs.calculate_coeficients()
+            if e.key == pygame.K_LEFT:
+                if fs.n > 0: fs.n -= 1
+                fs.calculate_coeficients()
         elif e.type == pygame.KEYUP:
-            if e.key == pygame.K_z:
+            if e.key == pygame.K_UP:
                 press_z = -1
-            if e.key == pygame.K_x:
+            if e.key == pygame.K_DOWN:
                 press_x = -1
-    
+
     #  hold the key
     if press_z > 0 and current_time - press_z > HOLD_AFTER:
-        ft.zoom += zoom_amount
+        fs.zoom += zoom_amount
     if press_x > 0 and current_time - press_x > HOLD_AFTER:
-        if ft.zoom - zoom_amount > 0:
-            ft.zoom -= zoom_amount
+        if fs.zoom - zoom_amount > 0:
+            fs.zoom -= zoom_amount
 
     # drawing points
     if STATE == 1 and current_time - prev_time >= SAMPLE_RATE:
         prev_time = current_time
-        ft.record_point()
+        fs.record_point()
 
     # run animation
     if STATE == 2 and current_time - prev_time >= UPDATE_RATE:
         prev_time = current_time
         canvas.display.fill(canvas.bg_color)
-        canvas.print_zoom(ft.zoom)
-        ft.calculate_point()
-        ft.draw_curve()
+        canvas.print_zoom(fs.zoom)
+        fs.calculate_point()
+        fs.draw_curve()
 
     pygame.display.update()
